d28959d3b8b6e7997bb90da0fd18ad7e8a1a81db
SovaV <sova.volodymyr95@gmail.com>
2021-12-13 16:58:27 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,9 +1,29 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const contacts = path.join(__dirname, './contacts.json')
+
 // const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contacts, 'utf8')
+    const result = JSON.parse(data)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contacts, 'utf8')
+    const contactsId = JSON.parse(data)
+    const getContact = contactsId.filter(({ id }) => id === contactId)
+    return getContact
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 const removeContact = async (contactId) => {}
 
