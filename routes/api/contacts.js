1a666ca9188957497a4f7ef2408a44c47cf047c4
SovaV <sova.volodymyr95@gmail.com>
2021-12-14 17:17:18 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const { listContacts, getContactById } = require('../../model/index')
-
+const product = require('../../model/contacts.json')
 router.get('/', async (req, res, next) => {
-  const getListContacts = await listContacts()
-  res.json({ getListContacts })
+  // const getListContacts = await listContacts()
+  // res.json(await listContacts())
+  res.json(product)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  const getById = await getContactById()
-  res.send(`${getById} ${req.params.contactId}`)
+  // const getById = await getContactById()
+  // res.send(`${getById} ${req.params.contactId}`)
+  const { id } = req.params
+  const result = product.find((item) => item.id === id)
+  res.json(result)
+
+  // res.json(await getContactById())
 })
 
 router.post('/', async (req, res, next) => {
