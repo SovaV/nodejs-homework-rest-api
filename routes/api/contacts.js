b4397215d44086785fa7c77f0ff26ca00ea4de2c
SovaV <sova.volodymyr95@gmail.com>
2021-12-23 17:57:11 +0200

    sub

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,12 @@
 const express = require('express')
 const { NotFound, BadRequest } = require('http-errors')
-const Joi = require('joi')
+const { joiShema } = require('../../model/contact')
 const router = express.Router()
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  updateContact,
-  removeContact,
-} = require('../../model/index')
-
-const joiShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-}).min(1)
+const { Contact } = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
   try {
-    res.json(await listContacts())
+    res.json(await Contact.find())
   } catch (error) {
     next(error)
   }
@@ -27,12 +15,15 @@ router.get('/', async (req, res, next) => {
 router.get('/:contactId', async (req, res, next) => {
   const { contactId } = req.params
   try {
-    const contacts = await getContactById(contactId)
+    const contacts = await Contact.findById(contactId)
     if (!contacts) {
       throw new NotFound()
     }
     res.json(contacts)
   } catch (error) {
+    if (error.message.includes('Cast to ObjectId failed')) {
+      error.status = 404
+    }
     next(error)
   }
 })
@@ -42,14 +33,13 @@ router.post('/', async (req, res, next) => {
     const { error } = joiShema.validate(body)
     if (error) {
       throw new BadRequest('missing required name field')
-      // const error = new Error('missing required name field')
-      // error.status = 400
-      // throw error
     }
-    const contacts = await addContact(body)
-    res.status(201).json(contacts)
-    console.log(req.body)
+    const newContacts = await Contact.create(body)
+    res.status(201).json(newContacts)
   } catch (error) {
+    if (error.message.includes('validation failed')) {
+      error.status = 400
+    }
     next(error)
   }
 })
@@ -57,7 +47,7 @@ router.post('/', async (req, res, next) => {
 router.delete('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params
-    const deleteContact = await removeContact(contactId)
+    const deleteContact = await Contact.findByIdAndRemove(contactId)
     if (!deleteContact) {
       throw new NotFound()
     }
@@ -69,23 +59,50 @@ router.delete('/:contactId', async (req, res, next) => {
 
 router.patch('/:contactId', async (req, res, next) => {
   try {
-    const { error } = joiShema.validate(req.body)
-    if (error) {
-      throw new BadRequest('missing fields')
-      // const error = new Error('missing fields')
-      // error.status = 400
-      // throw error
-    }
+    // const { error } = joiShema.validate(req.body)
+    // if (error) {
+    //   throw new BadRequest('missing fields')
+    // }
     const { contactId } = req.params
-    const updateContactById = await updateContact(contactId, req.body)
+    const updateContactById = await Contact.findByIdAndUpdate(
+      contactId,
+      req.body,
+      { new: true }
+    )
     if (updateContactById) {
       res.json(updateContactById)
     } else {
       throw new NotFound()
     }
   } catch (error) {
+    if (error.message.includes('validation failed')) {
+      error.status = 400
+    }
     next(error)
   }
 })
-
+router.patch('/:contactId', async (req, res, next) => {
+  try {
+    // const { error } = joiShema.validate(req.body)
+    // if (error) {
+    //   throw new BadRequest('missing fields')
+    // }
+    const { contactId } = req.params
+    const updateContactById = await Contact.findByIdAndUpdate(
+      contactId,
+      req.body,
+      { new: true }
+    )
+    if (updateContactById) {
+      res.json(updateContactById)
+    } else {
+      throw new NotFound()
+    }
+  } catch (error) {
+    if (error.message.includes('validation failed')) {
+      error.status = 400
+    }
+    next(error)
+  }
+})
 module.exports = router
