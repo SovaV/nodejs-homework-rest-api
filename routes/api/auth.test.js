cd9223792008e559761003d97efd981d82a462f6
SovaV <sova.volodymyr95@gmail.com>
2022-01-18 17:14:28 +0200

    sub

--- routes/api/auth.test.js
+++ routes/api/auth.test.js
@@ -1,9 +1,10 @@
-const request = require('supertest')
-const app = require('../../app')
 const mongoose = require('mongoose')
+const request = require('supertest')
 require('dotenv').config()
 
+const app = require('../../app')
 const { User } = require('../../model/user')
+
 const { DB_TEST_HOST } = process.env
 
 describe('test auth', () => {
@@ -20,22 +21,26 @@ describe('test auth', () => {
       mongoose.connection.close(() => done())
     })
   })
-  test('test register rout', () => {
+
+  test('test register route', async () => {
     const registerData = {
+      name: 'Test',
       email: 'Test1@ukr.net',
       password: '123123',
     }
+
     const response = await request(app)
       .post('/api/auth/register')
       .send(registerData)
 
     // check response
     expect(response.statusCode).toBe(201)
-    expect(response.body.message).toBe('Register success')
+    expect(response.body.message).toBe('Register successs')
 
     // check data in database
     const user = await User.findById(response.body._id)
     expect(user).toByThruthy()
+    expect(user.name).toBe(registerData.name)
     expect(user.email).toBe(registerData.email)
   })
 })
